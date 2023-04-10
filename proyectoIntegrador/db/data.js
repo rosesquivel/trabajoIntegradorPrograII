7c2d6059f044621756dc71cf6928616d32b78f7b
aldanagarnero <agarnero@udesa.edu.ar>
2023-04-10 15:11:29 -0300

    CSS

--- proyectoIntegrador/db/data.js
+++ proyectoIntegrador/db/data.js
@@ -71,98 +71,102 @@ let data = {
         {
             id: 1,
             name: 'GLOSS BOMB',
-            longDescription: 'El brillo de labios híbrido brinda un toque de tinte y de apariencia más completa. El innovador voluminizador contiene una mezcla de ingredientes, que incluyen aceite de raíz de jengibre y extracto de fruta de pimienta, diseñado para brindar una boca innegablemente espesa. La rica manteca de karité acondiciona mientras que el brillo húmedo maximiza el volumen visible. Es hora del efecto completo',
-            shortDescription:'Labial voluminizador color nude rosado',
-            color: 'Nude Rosado',
-            image:'../images/products/makeup/2.png'
+            longDescription: 'El brillo de labios híbrido brinda un toque de tinte y de apariencia más completa. El innovador voluminizador contiene una mezcla de ingredientes, que incluyen aceite de raíz de jengibre y extracto de fruta de pimienta, diseñado para brindar una boca innegablemente espesa. La manteca de karité acondiciona mientras que el brillo húmedo maximiza el volumen visible. Es hora del efecto completo.',
+            shortDescription:'Labial voluminizador color marrón',
+            color: 'Marrón',
+            image:'../images/makeup/1.png'
         },
         {
             id: 2,
-            name: 'GLOSS BOMB',
-            longDescription: 'El brillo de labios híbrido brinda un toque de tinte y de apariencia más completa. El innovador voluminizador contiene una mezcla de ingredientes, que incluyen aceite de raíz de jengibre y extracto de fruta de pimienta, diseñado para brindar una boca innegablemente espesa. La rica manteca de karité acondiciona mientras que el brillo húmedo maximiza el volumen visible. Es hora del efecto completo',
-            shortDescription:'Labial voluminizador color marrón',
-            color: 'Marrón',
-            image:'../images/products/makeup/1.png'
+            name: 'SNAP SHADOWS',
+            longDescription: 'Una revolucionaria minipaleta de sombras de ojos portátil de 6 tonos ricos en una gama de acabados mate a brillante. Los colores son intensos y fáciles de mezclar. Combiná tus favoritos.',
+            shortDescription: 'Paleta portable de sombras color nudes durazno cálido',
+            color: 'Nudes Durazno Cálido',
+            image:'../images/makeup/8.png'
         },
         {
             id: 3,
-            name: 'SNAP SHADOWS',
-            longDescription: 'Una revolucionaria minipaleta de sombras de ojos portátil de 6 tonos ricos en una gama de acabados mate a brillante. Los colores son intensos y fáciles de mezclar. Combiná tus favoritos',
-            shortDescription: 'Paleta portable de sombras color nudes durazno cálido',
-            color: 'Nudes Durazno Cálido',
-            image:'../images/products/makeup/8.png'
+            name: "BASE PRO FILT'R",
+            longDescription: 'Una base mate suave de larga duración que cuenta con tecnología adaptable al clima para combatir el calor, el sudor y el brillo. Salí todo el día o toda la noche, tu base siempre estará fresca.',
+            shortDescription:'Base matte de larga duración color profundo',
+            color: 'Profundo',
+            image:'../images/makeup/5.png'
         },
         {
             id: 4,
-            name: 'SNAP SHADOWS',
-            longDescription: 'Una revolucionaria minipaleta de sombras de ojos portátil de 6 tonos ricos en una gama de acabados mate a brillante. Los colores son intensos y fáciles de mezclar. Combiná tus favoritos',
-            shortDescription: 'Paleta portable de sombras color nudes clásicos',
-            color: 'Nudes Clásicos',
-            image:'../images/products/makeup/7.png'
+            name: "BRONZER",
+            longDescription: 'Un polvo bronceador de larga duración y resistente a la transpiración. Brillo instantáneo, combinable y fácil de usar.',
+            shortDescription:'Polvo bronzeador color marrón medium',
+            color: 'Marrón Medium',
+            image:'../images/makeup/4.png'
         },
         {
             id: 5,
-            name: "BASE PRO FILT'R",
-            longDescription: 'Una base mate suave de larga duración que cuenta con tecnología adaptable al clima para combatir el calor, el sudor y el brillo. Salí todo el día o toda la noche, tu base siempre estará fresca',
-            shortDescription: 'Base matte de larga duración color claro medium',
-            color: 'Claro Medium',
-            image:'../images/products/makeup/6.png'
+            name: 'HIGHLIGHTER',
+            longDescription: 'Un iluminador híbrido en crema-polvo de larga duración y ligero con un acabado hipermetálico, en una colorida gama de tonos que desafían las reglas diseñados para resaltar todos los tonos de piel. Infinitas formas de brillar.',
+            shortDescription: 'Iluminador crema-polvo color rubí',
+            color: 'Rubí',
+            image:'../images/makeup/3.png'
         },
         {
             id: 6,
             name: "BASE PRO FILT'R",
-            longDescription: 'Una base mate suave de larga duración que cuenta con tecnología adaptable al clima para combatir el calor, el sudor y el brillo. Salí todo el día o toda la noche, tu base siempre estará fresca',
-            shortDescription:'Base matte de larga duración color profundo',
-            color: 'Profundo',
-            image:'../images/products/makeup/5.png'
+            longDescription: 'Una base mate suave de larga duración que cuenta con tecnología adaptable al clima para combatir el calor, el sudor y el brillo. Salí todo el día o toda la noche, tu base siempre estará fresca.',
+            shortDescription: 'Base matte de larga duración color claro medium',
+            color: 'Claro Medium',
+            image:'../images/makeup/6.png'
         },
+        
         {
             id: 7,
-            name: "BRONZER",
-            longDescription: 'Un polvo bronceador de larga duración y resistente a la transpiración. Brillo instantáneo, combinable y fácil de usar',
-            shortDescription:'Polvo bronzeador color marrón medium',
-            color: 'Marrón Medium',
-            image:'../images/products/makeup/4.png'
+            name: 'GLOSS BOMB',
+            longDescription: 'El brillo de labios híbrido brinda un toque de tinte y de apariencia más completa. El innovador voluminizador contiene una mezcla de ingredientes, que incluyen aceite de raíz de jengibre y extracto de fruta de pimienta, diseñado para brindar una boca innegablemente espesa. La manteca de karité acondiciona mientras que el brillo húmedo maximiza el volumen visible. Es hora del efecto completo.',
+            shortDescription:'Labial voluminizador color nude rosado',
+            color: 'Nude Rosado',
+            image:'../images/makeup/2.png'
         },
         {
             id: 8,
-            name: 'HIGHLIGHTER',
-            longDescription: 'Un iluminador híbrido en crema-polvo de larga duración y ligero con un acabado hipermetálico, en una colorida gama de tonos que desafían las reglas diseñados para resaltar todos los tonos de piel. Infinitas formas de brillar',
-            shortDescription: 'Iluminador crema-polvo color rubí',
-            color: 'Rubí',
-            image:'../images/products/makeup/3.png'
+            name: 'SNAP SHADOWS',
+            longDescription: 'Una revolucionaria minipaleta de sombras de ojos portátil de 6 tonos ricos en una gama de acabados mate a brillante. Los colores son intensos y fáciles de mezclar. Combiná tus favoritos.',
+            shortDescription: 'Paleta portable de sombras color nudes clásicos',
+            color: 'Nudes Clásicos',
+            image:'../images/makeup/7.png'
         },
+        
+        
+        
         {
             id: 9,
             name: 'LIPSTICK ICON',
-            longDescription: 'El lipstick semi-matte fue creado con una fórmula para hacer que tus labios se vean cremosos, suaves y voluminosos. Su efecto duradero y color cautivante hace de éste el labial perfecto',
+            longDescription: 'El lipstick semi-matte fue creado con una fórmula para hacer que tus labios se vean cremosos, suaves y voluminosos. Su efecto duradero y color cautivante hace de éste el labial perfecto.',
             shortDescription: 'Labial semi-matte color marrón',
             color: 'Marrón',
-            image:'../images/products/makeup/lipstickBrown.png'
+            image:'../images/makeup/10.png'
         },
         {
             id: 10,
             name: 'LIPSTICK ICON',
-            longDescription: 'El lipstick semi-matte fue creado con una fórmula para hacer que tus labios se vean cremosos, suaves y voluminosos. Su efecto duradero y color cautivante hace de éste el labial perfecto',
+            longDescription: 'El lipstick semi-matte fue creado con una fórmula para hacer que tus labios se vean cremosos, suaves y voluminosos. Su efecto duradero y color cautivante hace de éste el labial perfecto.',
             shortDescription: 'Labial semi-matte color rosa profundo',
             color: 'Rosa profundo',
-            image:'../images/products/makeup/lipstickDeepPink.png'
+            image:'../images/makeup/12.png'
         },
         {
             id: 11,
             name: 'LIPSTICK ICON',
-            longDescription: 'El lipstick semi-matte fue creado con una fórmula para hacer que tus labios se vean cremosos, suaves y voluminosos. Su efecto duradero y color cautivante hace de éste el labial perfecto',
+            longDescription: 'El lipstick semi-matte fue creado con una fórmula para hacer que tus labios se vean cremosos, suaves y voluminosos. Su efecto duradero y color cautivante hace de éste el labial perfecto.',
             shortDescription: 'Labial semi-matte color rojo',
             color: 'Rojo',
-            image:'../images/products/makeup/lipstickRed.png'
+            image:'../images/makeup/9.png'
         },
         {
             id: 12,
             name: 'LIPSTICK ICON',
-            longDescription: 'El lipstick semi-matte fue creado con una fórmula para hacer que tus labios se vean cremosos, suaves y voluminosos. Su efecto duradero y color cautivante hace de éste el labial perfecto',
+            longDescription: 'El lipstick semi-matte fue creado con una fórmula para hacer que tus labios se vean cremosos, suaves y voluminosos. Su efecto duradero y color cautivante hace de éste el labial perfecto.',
             shortDescription: 'Labial semi-matte color rosa claro',
             color: 'Rosa claro',
-            image:'../images/products/makeup/lipstickLightPink.png'
+            image:'../images/makeup/11.png'
         }
     ],
     comments: [
